2dea615a7bc4af3a3fa1f16e05bd3050bb77585b
Polcsi <pollakbence12@gmail.com>
2022-02-22 19:41:18 +0100

    Created Frontend Routes and Responsive Navigation

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,35 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// Import Pages
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Appointments from "./pages/Appointments";
+import Administrators from "./pages/Administrators";
+import Customers from "./pages/Customers";
+
+// Componenets
+import Navbar from "./components/Navbar";
+// Style Sheets
+import "./css/main.css";
+import "./css/nav.css";
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <Router>
+        <div className="container">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/customers" element={<Customers />} />
+            <Route path="/appointments" element={<Appointments />} />
+            <Route path="/administrators" element={<Administrators />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
+  );
 }
 
 export default App;
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,95 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+// Icons
+import { BsFillPersonFill, BsPeople } from "react-icons/bs";
+import { BiMenuAltRight } from "react-icons/bi";
+import { FaTimes } from "react-icons/fa";
+import { MdOutlineLogout } from "react-icons/md";
+import { AiOutlineFieldTime, AiOutlineHome } from "react-icons/ai";
+import { RiAdminLine } from "react-icons/ri";
+
+const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+
+  const handlingNavClasses = () => {
+    let classes = "navbar";
+    if (showLinks) {
+      classes += " show";
+      document.body.classList.add("disabledScroll");
+    } else {
+      document.body.classList.remove("disabledScroll");
+    }
+    return classes;
+  };
+
+  return (
+    <nav className={handlingNavClasses()}>
+      <div className="content">
+        <div className="nav-header">
+          <div className="profile-picture">
+            <BsFillPersonFill />
+          </div>
+          <h3 className="greeting">Hi</h3>
+          <h3 className="actual-user">Hurka Cica</h3>
+        </div>
+        <div
+          className={
+            showLinks ? "icon cancel-btn show" : "icon cancel-btn hide"
+          }
+        >
+          <FaTimes onClick={() => setShowLinks(false)} />
+        </div>
+        <div
+          className={showLinks ? "icon menu-btn hide" : "icon menu-btn show"}
+        >
+          <BiMenuAltRight onClick={() => setShowLinks(true)} />
+        </div>
+        <ul className="menu-list">
+          <li>
+            <Link
+              className="nav-link"
+              to="/"
+              onClick={() => setShowLinks(false)}
+            >
+              <AiOutlineHome /> Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="nav-link"
+              to="/customers"
+              onClick={() => setShowLinks(false)}
+            >
+              <BsPeople /> Customers
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="nav-link"
+              to="/administrators"
+              onClick={() => setShowLinks(false)}
+            >
+              <RiAdminLine /> Administrators
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="nav-link"
+              to="/appointments"
+              onClick={() => setShowLinks(false)}
+            >
+              <AiOutlineFieldTime /> All Appointments
+            </Link>
+          </li>
+          <li>
+            <button className="nav-link" onClick={() => setShowLinks(false)}>
+              <MdOutlineLogout /> Logout
+            </button>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
